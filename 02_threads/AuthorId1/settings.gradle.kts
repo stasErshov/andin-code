258e12481142e411b3085ad83d319f604bb93413
DA <stas228390@mail.ru>
2023-04-04 17:45:59 +0300

    Домашнее задание к занятию «3.1. Coroutines: легковесные потоки, Thread Pools & Dispatchers»

---
+++ 02_threads/AuthorId1/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "AuthorId1"
+
---
+++ 02_threads/AuthorId1/src/main/kotlin/api.kt
@@ -0,0 +1,46 @@
+data class Attachment(
+    val url: String,
+    val description: String,
+    val type: AttachmentType,
+)
+
+data class Author(
+    val id: Long,
+    val name: String,
+    val avatar: String,
+)
+
+data class Comment(
+    val id: Long,
+    val postId: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+)
+
+data class CommentAuthor(
+    val author: Author,
+    val comment: Comment
+)
+
+data class Post(
+    val id: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    var attachment: Attachment? = null,
+)
+
+data class FullPost(
+    val author: Author,
+    val post: Post,
+    val comments: List<CommentAuthor>,
+)
+
+enum class AttachmentType {
+    VIDEO, IMAGE, STRING
+}
